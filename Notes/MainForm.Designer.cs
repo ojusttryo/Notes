5144bf69550fc2ebeca7381a1f7b43db8290a81a
JustTry <ojusttryo@mail.ru>
2018-12-26 13:57:02 +0300

    Rename main form, set size, change NET framework to 4.0

--- Notes/MainForm.Designer.cs
+++ Notes/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Notes
 {
-	partial class Form1
+	partial class MainForm
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,9 +28,18 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.SuspendLayout();
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(784, 462);
+			this.MinimumSize = new System.Drawing.Size(800, 500);
+			this.Name = "MainForm";
+			this.Text = "Notes";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
